6c91e54f48de8fa583670758fdae2b51ca9fac0a
Bhaskar <bhaskarchavan2001@gmail.com>
2023-09-27 19:08:23 +0530

    first commit

---
+++ App.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+# Download the 'punkt' tokenizer data
+nltk.download('punkt')
+
+# Download the 'stopwords' data
+nltk.download('stopwords')
+
+ps = PorterStemmer()
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title("Email Spam Classifier")
+
+input_mail = st.text_area("Enter the mail")
+
+if st.button('Predict'):
+
+    # 1. preprocessing
+    transformed_mail = transform_text(input_mail)
+    # 2. vectorization
+    vector_input = tfidf.transform([transformed_mail])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("The above mail is spam")
+    else:
+        st.header("The above mail is not spam")
